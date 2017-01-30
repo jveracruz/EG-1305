d556c8b70218799cf3de4de92a8c6ad468b6449a
jveracruz <jveracruz@mail.stmarytx.edu>
2017-01-29 20:05:56 -0600

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,17 +1,5 @@
 import java.text.DecimalFormat;
 
-// DecimalFormat formatter = new DecimalFormat(#0.00);
-// ^Class name^  ^objectref^ ^keyword to instantiate objects^ (DecimalFormat) is constructor
-//which also instantiate object 
-//System.out.println("radius+"formatter(radius)
-// ("Area is" + formatter(area));
-//"perimeter is" + formatter (perimeter);
-// println(formatter.format(area));
-//Objectref.method or field
-//         ^dot operator
-//Math.PI
-//^Class name "." dot operator "PI" constant
-
 
 public class Circle {
 	public static void main(String[] args){
