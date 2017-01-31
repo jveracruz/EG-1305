be135bd18b8fc86217c213b951bfcc013a7a49cc
jveracruz <jveracruz@mail.stmarytx.edu>
2017-01-30 22:18:04 -0600

    Cleaner
    made a better code

--- StockTransaction.java
+++ StockTransaction.java
@@ -3,19 +3,21 @@ import java.text.DecimalFormat;
 public class StockTransaction {
 	public static void main(String[] args)
 	{
-		int JoeShares = 1000;
-		double perShare = JoeShares*(32.87);
-		final double COMMISION_RATE = perShare*(0.02);
-		int soldShares = 1000;
-		double sellPrice = soldShares*(33.92);
-		final double soldCommision = sellPrice*(0.02);
-		double profit = sellPrice - COMMISION_RATE - soldCommision - perShare;
+		final double COMMISSION_RATE = .02;
+		
+		int joeShares = 1000;
+		double totalShare = joeShares*(32.87);
+		double brokerPay1 = totalShare*COMMISSION_RATE;
+		
+		double sellPrice = joeShares*(33.92);
+		double brokerPay2 = sellPrice*COMMISSION_RATE;
+		double profit = (sellPrice + brokerPay2) - (brokerPay1 + totalShare);
 		DecimalFormat formatter = new DecimalFormat("0.00");
 		
-		System.out.println("The ammount of money Joe paid for the stock was $"+formatter.format(perShare));
-		System.out.println("The ammount Joe paid his stockbroker after buying stock was $"+formatter.format(COMMISION_RATE));
+		System.out.println("The ammount of money Joe paid for the stock was $"+formatter.format(totalShare));
+		System.out.println("The ammount Joe paid his stockbroker after buying stock was $"+formatter.format(brokerPay1));
 		System.out.println("The ammount that Joe sold for his stock was $"+formatter.format(sellPrice));
-		System.out.println("The ammount that Joe paid his stockbroker when he sold his stock was $"+formatter.format(soldCommision));
+		System.out.println("The ammount that Joe paid his stockbroker when he sold his stock was $"+formatter.format(brokerPay2));
 		System.out.println("The ammount of money Joe had for profit was $"+formatter.format(profit));
 	}
 
