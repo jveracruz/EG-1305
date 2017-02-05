f8b4ad0e5319a1d5fb0b85e05c23803fa0d09d4a
aktunc-STMU <aktunc@gmail.com>
2017-02-05 01:08:05 -0600

    Check comment

--- Circle.java
+++ Circle.java
@@ -4,8 +4,8 @@ import java.text.DecimalFormat;
 public class Circle {
 	public static void main(String[] args){
 		double radius = 5;
-		double Area = radius*radius*Math.PI;
-		double Perimeter = 2*radius*Math.PI;
+		double Area = radius*radius*Math.PI; //Don't start a variable with a capital letter
+		double Perimeter = 2*radius*Math.PI; //Don't start a variable with a capital letter
 		DecimalFormat formatter = new DecimalFormat("#0.00");
 		System.out.println("Area is "+formatter.format(Area));
 		System.out.println("Perimeter is "+formatter.format(Perimeter));
