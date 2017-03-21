11b3013c31b0f4c786f4c7440ea81e25419cae9f
jveracruz <jveracruz@mail.stmarytx.edu>
2017-03-21 00:01:24 -0500

    Add files via upload

---
+++ Chess.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+
+public class Chess {
+
+	public static void main(String[] args){
+		
+		long wheat = 0;
+		long chess;
+		long total;
+		
+		for(int i = 0; i < 64; i++){
+			
+			chess = (long) Math.pow(2 , i);
+			
+			wheat = wheat + chess;
+			
+			BigInteger hugeNumber = new BigInteger("" + wheat);
+			
+			System.out.println(hugeNumber);
+			
+		}
+
+		//total = 2 + wheat;
+		
+		//System.out.println("Total on chessboard is: " + total);
+	}
+}
