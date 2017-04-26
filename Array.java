352b246ae700293f684c115ac9b5186305dda222
jveracruz <jveracruz@mail.stmarytx.edu>
2017-04-26 00:52:35 -0500

    Changed code by including correct methods

--- Array.java
+++ Array.java
@@ -7,8 +7,6 @@ public class Array {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int sum = 0;
-		
 		System.out.println("How many number would you like in the array?");
 		int userNum = input.nextInt();
 		
@@ -16,55 +14,96 @@ public class Array {
 		
 		int[] userArray = new int[userNum];
 		
-		for (int i = 0; i < userArray.length; i++) {
-			
-			System.out.print("Enter number " + (i + 1) + ": ");
-			userArray[i] = input.nextInt();
-			
-		}
+		getValues(userArray);
 		
-		for (int i : userArray) {
-			
-			sum += i;
-			
-		}
+		showTotal(userArray);
 		
-		int highest = userArray[0];
+		showHighest(userArray);
 		
-		for (int i = 0; i < userArray.length; i++) {
-			
-			if(userArray[i] > highest) {
-				
-				highest = userArray[i];
-				
-			}
-			
-		}
-		
-		int lowest = userArray[0];
-		
-		for (int i = 0; i < userArray.length; i++) {
-			
-			if(userArray[i] < lowest) {
-				
-				lowest = userArray[i];
-				
-			}
-			
-		}
-		
-		double average = sum / userArray.length;
+		showLowest(userArray);
 		
 		System.out.println("Your array is " + Arrays.toString(userArray));
 		
-		System.out.println("The lowest element is " + lowest);
+		System.out.println("The lowest element is " + showLowest(userArray));
 		
-		System.out.println("The highest element is " + highest);
+		System.out.println("The highest element is " + showHighest(userArray));
 		
-		System.out.println("The sum of the elements is " + sum);
+		System.out.println("The sum of the elements is " + showTotal(userArray));
 		
-		System.out.println("The average of the elements is " + average);
+		System.out.println("The average of the elements is " + showAverage(userArray));
 		
-	}
-
+		}
+		
+		public static void getValues(int[] userArray)
+		{
+			Scanner input = new Scanner(System.in);
+			
+			for (int i = 0; i < userArray.length; i++) {
+				
+				System.out.print("Enter number " + (i + 1) + ": ");
+				userArray[i] = input.nextInt();
+				
+			}
+		}
+		
+		public static int showTotal(int[] userArray) {
+			
+			int sum = 0;
+			
+			for (int i : userArray) {
+				
+				sum += i;
+				
+			}
+			
+			return sum;
+			
+		}
+		
+		public static int showHighest(int[] userArray) {
+			
+			int highest = userArray[0];
+			
+			for (int i = 0; i < userArray.length; i++) {
+				
+				if(userArray[i] > highest) {
+					
+					highest = userArray[i];
+					
+				}
+				
+			}
+			
+			return highest;
+			
+		}
+		
+		public static int showLowest(int[] userArray) {
+			
+			int lowest = userArray[0];
+			
+			for (int i = 0; i < userArray.length; i++) {
+				
+				if(userArray[i] < lowest) {
+					
+					lowest = userArray[i];
+					
+				}
+				
+			}
+			
+			return lowest;
+			
+		}
+		
+		public static double showAverage(int[] userArray) {
+			
+			double average = showTotal(userArray) / userArray.length;
+			
+			return average;
+		}
+		
+		
 }
+
+
